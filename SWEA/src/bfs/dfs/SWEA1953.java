d1034115d84a442874546d967eb95c5a122aba02
JuyoungKimmy-Kim <kimmykim.jy@gmail.com>
2022-09-28 17:58:46 +0900

    220928

--- SWEA/src/bfs/dfs/SWEA1953.java
+++ SWEA/src/bfs/dfs/SWEA1953.java
@@ -12,9 +12,10 @@ public class SWEA1953 {
 	static final int dy[] = {0,0,1,-1};
 	static final int dx[] = {1,-1,0,0};
 	
-	static int T,N,M,R,C,L;
+	static int T,N,M,R,C,L,ans;
 	static int[][] map;
 	static boolean[][] visited;
+	static Queue<int[]> q;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader (new InputStreamReader (System.in));
@@ -31,6 +32,7 @@ public class SWEA1953 {
 			
 			map=new int[N][M];
 			visited=new boolean[N][M];
+			q=new ArrayDeque<>();
 			
 			for (int i=0; i<N; i++) {
 				st=new StringTokenizer (br.readLine());
@@ -39,20 +41,21 @@ public class SWEA1953 {
 				}
 			}
 			
-
-			System.out.println("#"+tc+" "+bfs());
+			ans=0;
+			bfs();
+			System.out.println("#"+tc+" "+ans);
 			
 		}
 
 	}
 	
-	static int bfs () {
-		int ans=0;
-		
-		Queue<int[]> q=new ArrayDeque<>();
+	static void bfs () {
+
 		q.add(new int[] {R,C});
 		visited[R][C]=true;
 		ans++;
+		
+		int ny=0, nx=0;
 		
 		while (!q.isEmpty()) {
 			int y=q.peek()[0];
@@ -62,32 +65,95 @@ public class SWEA1953 {
 			switch (type) {
 			case 1: 
 				for (int d=0; d<4; d++) {
-					int ny=y+dy[d];
-					int nx=x+dx[d];
-					
-					if (isInRange (ny,nx) && !visited[ny][nx]) {
-						ans++;
-						visited[ny][nx]=true;
-						q.add(new int[] {ny, nx});
-					}
+					ny=y+dy[d];
+					nx=x+dx[d];
+					move(ny, nx);
 				}
 				break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
-			case 5: break;
-			case 6: break;
-			case 7: break;
-			}
-			
+			case 2: 
+				for (int d=2; d<4; d++) {
+					ny=y+dy[d];
+					nx=x+dx[d];
+
+					move(ny, nx);
+				}
+				break;
+			case 3: 
+				for (int d=0; d<2; d++) {
+					ny=y+dy[d];
+					nx=x+dx[d];
+					
+					move(ny,nx);
+				}
+				break;
+			case 4: 
+				ny=y+dy[0];
+				nx=x+dx[0];
+				move(ny, nx);
+				
+				ny=y+dy[3];
+				nx=x+dx[3];
+				move(ny, nx);
+				
+				break;
+			case 5: 
+				ny=y+dy[0];
+				nx=x+dx[0];
+				move(ny, nx);
+				
+				ny=y+dy[2];
+				nx=x+dx[2];
+				move(ny, nx);
+				
+				break;
+			case 6: 
+				
+				ny=y+dy[1];
+				nx=x+dx[1];
+				move(ny, nx);
+				
+				ny=y+dy[2];
+				nx=x+dx[2];
+				move(ny, nx);
+				
+				break;
+			case 7: 
+				ny=y+dy[1];
+				nx=x+dx[1];
+				move(ny, nx);
+				
+				ny=y+dy[3];
+				nx=x+dx[3];
+				move(ny, nx);
+				
+				break;
+			}	
 		}
-		
-		return ans;
+	}
+	
+	static void move (int ny, int nx) {
+		if (isInRange (ny,nx) && !visited[ny][nx]) {
+			ans++;
+			visited[ny][nx]=true;
+			q.add(new int[] {ny, nx});
+			
+			print();
+		}
 	}
 	
 	static boolean isInRange (int y, int x) {
 		if (y<0 || x<0 || y>=N || x>=M) return false;
 		return true;
+	}
+	
+	static void print () {
+		System.out.println("===============================");
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<M; j++) {
+				System.out.print(visited[i][j] ? "O " : "X ");
+			}
+			System.out.println();
+		}
 	}
 
 }
